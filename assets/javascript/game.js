86b058dfd68216f97bb13826033aa518d2435646
RaMbo8315 <30378848+RaMbo8315@users.noreply.github.com>
2017-11-25 03:47:58 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,85 @@
 
 
 
-    var characters = {
-        rick: {
+    var characters = [
+          {
             "name": "rick",
             "hp": 150,
             "attack": 10,
             "counter-attack": 20,
-            "container": $("<div>"),
+            "container": $("<div class='character' value='rick'>"),
             "picture": "assets/images/rick.jpg",
-        },
+         },
 
-        spike: {
+         {
             "name": "spike",
             "hp": 100,
             "attack": 5,
             "counter-attack": 5,
-            "container": $("<div>"),
-            "picture": "assets/images/spike.jpg",
+            "container": $("<div class='character' value='spike'>"),
+            "picture": "assets/images/spikezom.jpg",
         },
 
-        governor: {
+         {
             "name": "governor",
             "hp": 180,
             "attack": 15,
             "counter-attack": 10,
-            "container": $("<div>"),
+            "container": $("<div class='character' value='governor'>"),
             "picture": "assets/images/governor.jpg",
         },
 
-        negan: {
+        {
             "name": "negan",
             "hp": 200,
             "attack": 20,
             "counter-attack": 15,
-            "container": $("<div>"),
+            "container": $("<div class='character' value='negan'>"),
             "picture": "assets/images/negan.jpg",
         },
-    }
+    ]
+         
 
-    console.log(characters.negan.hp);
-
-
-
-    begin();
+    console.log(characters[1].picture);
+  
 $(document).ready(function () {
 
     var characterId = ["#rick", "#spike", "#governor", "#negan"];
     var yourCharacter;
     var characterChosen;
-    var characterValue;
     var enemieCharacter;
     var enemieCharacterChosen;
     var defender;
     var healthPoints;
     var attack;
 
+    begin();
+    
+    displayCharacter();
+
+
         function createCharacter (i, location) {
          $(characters[i].container).empty();
-        var char = character[i].container;
-        var charName = $("<p>").text(characters[i].name);
-        var pic = $("<image>").attr('src', characters[i].picture);
-        var charHp =  $("<p>").text(characters[i].hp);
-        char.append(charName).append(pic).append(charHp);
+        var char = characters[i].container;
+        var charName = $("<p class='charName'>").text(characters[i].name);
+        var pic = $("<img>").attr('src', characters[i].picture);
+        var charHp = $("<p class='charHp'>").text("HP: " + characters[i].hp);
+        var charDiv = char.append(charName).append(pic).append(charHp);
         $(charDiv).appendTo(location);
     };
 
-    createCharacter(1, "#character-select");
+    function displayCharacter () {
+        for (var j = 0; j < characters.length; j++) {
+            createCharacter(j, "#character-select");
+        }
+    };
 
-    $("#rick").append(characters.rick["hp"]);
-    $("#spike").append(characters.spike["hp"]);
-    $("#governor").append(characters.governor["hp"]);
-    $("#negan").append(characters.negan["hp"]);
+    $("#character-select").on("click", function() {
+        characterChosen = this.value;
+        if (!enemieCharacterChosen) {
+            displayCharacter(j, "#enemiesToAttack");
+        }
+    });
 
     function restart() {
         yourCharacter = "";
@@ -83,12 +91,7 @@ $(document).ready(function () {
         healthPoints = "";
         attack = "";
     };
-        $("#rick").removeClass("ebutton").appendTo("#character-select");
-        $("#spike").removeClass("ebutton").appendTo("#character-select");
-        $("#governor").removeClass("ebutton").appendTo("#character-select");
-        $("#negan").removeClass("ebutton").appendTo("#character-select");
-        $("#character-chosen, #enemiesToAttack, #defender").empty();
-    }
+    
 
     function begin() {
         $(".chbutton").on("click", function () {
@@ -123,8 +126,10 @@ $(document).ready(function () {
             console.log(characterValue);
 
         });
-    };
+    }; 
+
 
     $("#restart").on("click", function () {
         restart();
     });
+});
